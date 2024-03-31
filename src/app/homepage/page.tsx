1cdd2d0b95b4f4e033fe1a6137138475fc359e1c
Noreen <noumanabid012@gmail.com>
2024-03-31 05:52:43 +0500

    Linking

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+function homePage () {
+    return <div>
+        <br/>
+        <h1>This is Home Page of the next project.</h1>
+        <br/>
+        <a href="namepage">This is the link for Name Page.</a>
+    </div>
+}
+
+export default homePage;
---
+++ src/app/namepage/page.tsx
@@ -0,0 +1,8 @@
+function namePage () {
+    return <div>
+        <h1>This is Name Page</h1>
+        <a href="homepage">This is the link for Home Page.</a>
+    </div>
+}
+
+export default namePage;
